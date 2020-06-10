77f48b86c0a7779eb680cba1aa54b3aae93f1442
Jim Nervall <nervall@hotmail.com>
2020-06-10 10:06:26 +0200

    Updates

---
+++ precache-manifest.a736c4273e79135805ddedde26a4e502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc531686fcf96a12ec248033dc6b91e",
+    "url": "/mandatory-advanced-js4/static/media/alphabet-blocks.ccc53168.jpg"
+  },
+  {
+    "revision": "eb147c3d7ae77fe3eefb",
+    "url": "/mandatory-advanced-js4/static/js/runtime~main.78de8069.js"
+  },
+  {
+    "revision": "3cc67972aa3c15cedafc",
+    "url": "/mandatory-advanced-js4/static/js/main.582b8b30.chunk.js"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/mandatory-advanced-js4/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "3cc67972aa3c15cedafc",
+    "url": "/mandatory-advanced-js4/static/css/main.c79f22a1.chunk.css"
+  },
+  {
+    "revision": "9f78715ace3a9f132b0909103d477157",
+    "url": "/mandatory-advanced-js4/index.html"
+  }
+];
